e4de0613b30499ebe48340fd681d4895bbd31541
dahuangggg <dahuangggg@icloud.com>
2026-07-06 11:06:07 +0800

    chore: scaffold opencode headroom package

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+export { default, HeadroomNativePlugin } from "./plugin.js";
+export type { HeadroomPluginOptions } from "./config.js";
+export type {
+  CompressionEngine,
+  ToolOutputCompressionInput,
+  ToolOutputCompressionResult,
+} from "./engine/types.js";
